cbf5ca011369ffd56f726eabbb05aef2cd6deb42
Ashish Pandey <ashish199728@gmail.com>
2018-06-27 11:03:13 +0530

    function basics

---
+++ src/kt program set 2/#2.2 functions_basics.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+
+    var name: String     //name is declared as String type
+    name = "Zephyr"
+    display(name)       //here name is parameter passed
+
+}
+
+fun display(name: String){      //here name is argument and declares as String type
+    print("My name is: " + name)
+}
