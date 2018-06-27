5bfbcfe95006c496e4d949ca3c2dabb827e10e77
Ashish Pandey <ashish199728@gmail.com>
2018-06-27 11:02:23 +0530

    basic syntax

---
+++ src/kt program set 2/#2.1 basic_syntax.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+
+    var myString = "Hello World!" //var is to declare a variable
+    var num = 99
+    var decimal = 99.5
+    var myChar = 'a'
+    var bool = false
+
+    val myAnotherStr = "My constant string value"
+    //val is to declare a constant variable
+    //if its value is tried to change, will result in an error
+
+    println(myString)
+    println(num)
+    println(decimal)
+    println(myChar)
+    println(bool)
+    print(myAnotherStr)
+
+}
