df27ec711c6562aaae92e9dfa33e0b125e23781b
Ashish Pandey <ashish199728@gmail.com>
2018-06-29 01:26:26 +0530

    constructor from package

---
+++ src/kt program set 2/#2.6 import_constructor_from_package.kt
@@ -0,0 +1,8 @@
+import com.shreks.Person4
+
+fun main(args: Array<String>){
+
+    var obj = Person4("Zephyr")     //object obj of constructor class Person4 is made, which is called from inside package com.shreks
+    obj.display()
+
+}
