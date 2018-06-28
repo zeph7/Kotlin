d210a2a07f6f72c82c305727ad13607c26e0a6a0
Ashish Pandey <ashish199728@gmail.com>
2018-06-29 01:31:29 +0530

    data types

---
+++ src/kt program set 3/#3.2 data_types.kt
@@ -0,0 +1,26 @@
+// defining a value
+
+fun main(args: Array<String>){
+
+    var age: Int = 10
+    //or you can simply write
+    // var age  = 10
+
+    var b: Boolean = true
+    var f: Float = 97.4F
+    //F is necessary to denote for Float Data Type
+    var d: Double = 98.47
+
+    println(age)
+    println(b)
+    println(f)
+    print(d)
+}
+
+// Why I in Int and B in Boolean capital?
+
+/*
+because Kotlin has no primitive data types
+All Data Types in Kotlin -> are OBJECTS
+Actually everything in Kotlin is an OBJECT
+* */
